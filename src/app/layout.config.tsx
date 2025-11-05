b7e47cbb6967c0404296d0ebe85f1811a1c3c66b
MuratFaizulla <muratf345@gmail.com>
2025-11-05 13:14:17 +0500

    2.01 commit

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -1,13 +1,7 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 import { siteConfig } from '../../site.config';
 
-/**
- * Shared layout configurations
- *
- * you can configure layouts individually from:
- * Home Layout: app/(home)/layout.tsx
- * Docs Layout: app/projects/layout.tsx
- */
+
 export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
@@ -36,5 +30,6 @@ export const baseOptions: BaseLayoutProps = {
       url: '/projects',
       active: 'nested-url',
     },
+   
   ],
 };
